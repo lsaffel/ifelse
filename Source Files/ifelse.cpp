d7d1178b0fb3b5280fac3f5425fe91a33167c668
Lisa <llvvss@icloud.com>
2024-03-09 14:03:23 -0700

    simple if else statement

--- Source Files/ifelse.cpp
+++ Source Files/ifelse.cpp
@@ -6,7 +6,20 @@ int main()
     // user enters integer number
     // program writes out if that number is even or odd
 
-    cout << 2 % 2 << std::endl;
-    cout << 7 % 5 << std::endl;
+    // cout << 2 % 2 << std::endl;     // result is 0 = remainder from 2 / 2
+    // cout << 7 % 5 << std::endl;     // result is 2 = remainder from 7 / 5
 
+    int number;
+    cout << "Please enter a whole number:";
+    cin >> number;
+
+    if (number % 2 == 0) 
+    {
+        cout << "You have entered an even number." << endl;
+    }
+    else
+    {
+        cout << "You have entered an odd number." << endl;
+    }
+    cout << "Thanks. Bye.";
 }
